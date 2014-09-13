dba9fe6bd1e0fe7c8305004e5acffccf6a0b79e9
Josh Karnofsky <jkarno@seas.upenn.edu>
2014-09-13 12:04:51 -0400

    new routes

--- Flock/routes/index.js
+++ Flock/routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var Facebook = require('facebook-node-sdk');
-var facebook = new Facebook({ appID: '722772554464882', secret: 'c7be44048e6a4571b8804ade0bac16da' });
-
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index');
@@ -13,20 +10,20 @@ router.get('/Login', function(req, res) {
 	res.render('login');
 });
 
-router.get('/Test', function(req,res) {
-	req.facebook.api('/me', function(err, data) {
-  	console.log(data);
-	});
-});
-
 router.get('/Register', function(req, res) {
 	res.render('Register')
 });
 
-router.get('/navbar', function(req,res){
+router.get('/Navbar', function(req,res){
 	res.render('navbar')
 });
 
-router.get('/Person/')
+router.get('/Person', function(req,res) {
+	res.render('person');
+})
+
+router.get('/Navbar1',function(req,res){
+	res.render('navbar1.hjs')
+});
 
 module.exports = router;
