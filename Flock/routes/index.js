55a4d44c1eab305c40f6bef6c4b77e16f54f5699
Josh Karnofsky <jkarno@seas.upenn.edu>
2014-09-13 12:56:55 -0400

    mailer basic functionality

--- Flock/routes/index.js
+++ Flock/routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mailer = require('../mailer');
 var router = express.Router();
 
 /* GET home page. */
@@ -25,5 +26,41 @@ router.get('/Person', function(req,res) {
 router.get('/Navbar1',function(req,res){
 	res.render('navbar1.hjs')
 });
+
+router.post('/RegisterNow', function(req, res, next) {
+  if (!req.body.Email || !req.body.FirstName || !req.body.LastName) {
+    console.log('You must provide an email address, first and last name.');
+    res.redirect('/Register');
+  }
+
+  var pin = makeid();
+
+  var mailOptions = {
+    from: 'The Flock Team <theflockteam@gmail.com>',
+    to: req.body.Email,
+    subject: 'Welcome to Flock!',
+    text: 'Welcome to Flock! A new way to enjoy the events you love. Please confirm your email with the following key: ' + pin
+  };
+
+  mailer.sendMail(mailOptions, function(err, info) {
+    if (err) {
+      next(err);
+    } else {
+      console.log('Message sent successfully!');
+      res.redirect('/Home');
+    }
+  });
+});
+
+function makeid()
+{
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for( var i=0; i < 5; i++ )
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
 
 module.exports = router;
