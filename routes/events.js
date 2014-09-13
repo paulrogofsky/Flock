110b51c8574e5beab1d6c5fb5668a83e9923c567
Josh Karnofsky <jkarno@seas.upenn.edu>
2014-09-13 17:33:27 -0400

    change logo font and color

--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 
+router.get('/CreateEvent',function(req,res,next) {
+	res.render('CreateEvent');
+});
 
 router.get('/CreateGroup', function(req, res) {
 	res.render('CreateGroup');
